4c4291f19961325e54a40d4fd0d2ce8ddad22dfa
Kevin Lin <klinime@berkeley.edu>
2020-05-22 23:38:47 +0800

    fixed pd truncate

--- mcts/cy_mcts.pyx
+++ mcts/cy_mcts.pyx
@@ -17,6 +17,9 @@ from libcpp.memory cimport unique_ptr, shared_ptr, weak_ptr, \
 from libcpp.string cimport string
 from cython.operator cimport dereference as deref
 
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
 cdef extern from "mcts.hpp" namespace "base" nogil:
     cdef cppclass Game:
         Game() except +
